0357d4e37b05baebb6e09c7ae94b5309f5ae339e
Billy Zhou <billyzs.728@gmail.com>
2018-11-03 00:36:03 -0400

    snaemk: add naive kmeans & unit tests

---
+++ include/snaemk/kmeans.hpp
@@ -0,0 +1,93 @@
+/// MIT License
+///
+/// Copyright (c) 2018 Billy Zhou
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <tuple>
+#include <vector>
+
+/// @brief compute max_iter counts of k-means on inputs ranging from i_begin to i_end, with initial
+/// centroids ranging from o_begin to o_end
+/// @tparam InputIter iterator for input data
+/// @tparam OutputIter iterator for centroids
+/// @tparam DistanceFn function that computes how far an input data point is from a centroid; must have signature
+/// RetType DistanceFn(const InputType&, const OutputType); RetType must have operator< defined; lesser value indicates
+/// closer proximity to a centroid
+/// @tparam AddOp how to accumulate InputIter to compute the new centroid
+/// @param i_begin start of input range
+/// @param i_end end of input range
+/// @param[in,out] o_begin start of centroid range
+/// @param[in,out] o_end end of centroid range
+/// @param distance_metric measure of how close an input is to an centroid
+/// @param AddOp how to accumulate centroid updates; defaults to std::plus for InputType
+/// @param init_val initial value for centroid update; defaults to a default-constructed InputType
+template<typename InputIter, typename OutputIter, typename DistanceFn,
+        typename InputType = typename std::iterator_traits<InputIter>::value_type,
+        typename AddOp = std::plus<InputType>>
+std::pair<bool, std::vector<size_t>>
+k_means(const InputIter i_begin, const InputIter i_end, const OutputIter o_begin, const OutputIter o_end,
+        const size_t max_iter,
+        DistanceFn distance_metric, /** call with distance_metric(const InputType&, const OType&) */
+        AddOp add = std::plus<InputType>(),
+        InputType init_val = InputType{}) {
+    using OutputType = typename std::iterator_traits<OutputIter>::value_type; // must be constructable from InputType
+    const auto num_input = static_cast<size_t>(std::distance(i_begin, i_end));
+    // associations[e] = c means the eth input is associated with the cth centroid
+    std::vector<size_t> associations(num_input,0); // initialize to 0
+    bool converged = false;
+
+    // execute
+    for (auto curr_iter = max_iter; !converged && curr_iter > 0; --curr_iter) {
+        // assign association
+        std::for_each(i_begin, i_end, [&, a_iter = associations.begin()](const InputType &elem) mutable {
+            const auto centroid_choice = static_cast<size_t>(
+                    std::distance(o_begin, std::min_element(o_begin, o_end,[&](const OutputType& c1, const OutputType& c2) {
+                        // note: distance() not O(1) if iters aren't RandomAccess
+                        return distance_metric(elem,c1) <= distance_metric(elem, c2);
+            })));
+            converged = (centroid_choice == std::exchange(*(a_iter++), centroid_choice));
+        });
+
+        if (converged) break;
+
+        // update centroid values
+        std::transform(o_begin, o_end, o_begin, [&, d = static_cast<size_t>(0)](const OutputType &discard) mutable {
+            (void) discard; // not used for now, but could early-terminate if the centroid's not moving much
+            InputType new_centroid_val = init_val;
+            // could do everything in one loop, but adding large numbers might overflow so count first & divide
+            const double factor = 1.0f / std::count(associations.cbegin(), associations.cend(), d);
+            std::for_each(associations.cbegin(), associations.cend(), [&, i_iter = i_begin](size_t a) mutable {
+                if (a == d) {
+                    new_centroid_val = add(new_centroid_val, (*i_iter) * factor); // assumes * is defined for InputType
+                }
+                i_iter++;
+            });
+            d++;
+            return new_centroid_val;
+        });
+    }
+
+    return std::make_pair(converged, std::move(associations));
+}
---
+++ test/test-kmeans.cpp
@@ -0,0 +1,87 @@
+#include <snaemk/kmeans.hpp>
+#include <gtest/gtest.h>
+#include <cmath>
+#include <future>
+#include <limits>
+#include <random>
+
+template <typename T>
+T l1_norm(T p1, T p2) {
+    return std::abs(p1 - p2);
+}
+
+TEST(k_means, trivial_1) {
+    constexpr std::array<float, 4> input{0,9,9,9};
+    std::array<float, 2> centroid{1.0f, 8.0f};
+    bool converged = false;
+    std::tie(converged, std::ignore) =
+            k_means(input.cbegin(), input.cend(), centroid.begin(), centroid.end(), 3, l1_norm<float>);
+    ASSERT_TRUE(converged);
+}
+
+TEST(k_means, trivial_2) {
+    constexpr std::array<float, 10> input{0,0,0,0,0,0,0,9,9,9};
+    std::array<float, 2> centroid{1.0f, 8.0f};
+    bool converged = false;
+    std::vector<size_t> associations{0,0,0,0,0,0,0,0,0,0};
+    const std::vector<size_t> expected_associations{0,0,0,0,0,0,0,1,1,1};
+    std::tie(converged, associations) = k_means(input.cbegin(), input.cend(),
+                                                centroid.begin(), centroid.end(),
+                                                std::numeric_limits<size_t>::max(),
+                                                l1_norm<float>);
+    ASSERT_TRUE(converged);
+    EXPECT_FLOAT_EQ(centroid[0], 0.0f);
+    EXPECT_FLOAT_EQ(centroid[1], 9.0f);
+    EXPECT_EQ(std::mismatch(associations.begin(), associations.end(),
+                            expected_associations.begin(), expected_associations.end()),
+              std::make_pair(associations.end(), expected_associations.end()));
+}
+
+TEST(k_means, moderate) {
+    constexpr long long clusters = 4;
+    constexpr long long bin_size = 10000;
+    constexpr auto sample_size = clusters * bin_size;
+    constexpr std::array<float, clusters> expected_centroid{10e9, 10e10, 10e11, 10e12};
+    auto centroid = expected_centroid;
+    std::array<float, sample_size> input;
+    constexpr float percent_radius = 0.1f;
+    std::random_device rd;
+    // fill initial guesses of centroids, and input, with random data
+    std::transform(centroid.begin(), centroid.end(), centroid.begin(),
+            [&input, &rd, i_input = input.begin()](const float& f) mutable {
+                std::uniform_real_distribution<float> rand(f*(1-percent_radius), f*(1+percent_radius));
+                i_input = std::generate_n(i_input, bin_size, [&](){return rand(rd);});
+                return rand(rd);
+    });
+    ASSERT_EQ(clusters*bin_size, input.size());
+
+    // shuffle a few times for good measure
+    std::shuffle(input.begin(), input.end(), std::mt19937(rd()));
+    std::shuffle(input.begin(), input.end(), std::mt19937(rd()));
+    std::shuffle(input.begin(), input.end(), std::mt19937(rd()));
+    std::shuffle(input.begin(), input.end(), std::mt19937(rd()));
+    std::shuffle(input.begin(), input.end(), std::mt19937(rd()));
+
+    std::vector<size_t> association(clusters * bin_size);
+    bool converged = false;
+    std::tie(converged, association) = k_means(input.cbegin(), input.cend(), centroid.begin(), centroid.end(), 10000,
+            l1_norm<float>);
+    ASSERT_TRUE(converged);
+    for (auto idx = 0; idx < clusters; idx++) {
+        EXPECT_EQ(bin_size, static_cast<long long>(std::count(association.begin(), association.end(), idx)));
+    }
+    for (size_t idx = 0; idx < input.size(); idx++) {
+        size_t actual = std::distance(expected_centroid.begin(),
+                std::min_element(expected_centroid.cbegin(), expected_centroid.cend(),
+                                 [elem = input.at(idx)](float c1, float c2){
+                                    return l1_norm(elem, c1) < l1_norm(elem, c2);
+        }));
+        ASSERT_EQ(actual, association.at(idx)) << "association for " << idx <<"th input (" << input[idx] << ") incorrect";
+    }
+    for (size_t idx = 0; idx < centroid.size(); idx++) {
+        EXPECT_LE(std::abs(centroid.at(idx) - expected_centroid.at(idx)) / expected_centroid.at(idx), percent_radius)
+        << idx << "th centroid failed; expected " << expected_centroid.at(idx) << " but got " << centroid.at(idx) << "\n"
+        << "centroids: " << centroid[0] << " " << centroid[1] << " " << centroid[2] << " " << centroid[3];
+    }
+}
+
