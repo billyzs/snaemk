7ed0df1e79683f29d27746176a94f6c95e3b2c14
Billy Zhou <billyzs.728@gmail.com>
2018-11-04 19:30:52 -0500

    add clang-format

--- test/distance-fns.hpp
+++ test/distance-fns.hpp
@@ -1,21 +1,23 @@
 #pragma once
+
 #include <cmath>
 #include <type_traits>
-namespace distance_fn {
-    template<typename T1 = void, typename T2 = void>
-    struct l1_norm {
-        constexpr auto operator()(const T1 &p1, const T2 p2) const {
-            using Ret = std::common_type_t<T1, T2>;
-            return std::abs(Ret{p1} - Ret{p2});
-        }
-    };
 
-    template<>
-    struct l1_norm<void, void> {
-        template<typename T1, typename T2>
-        constexpr auto operator()(T1 &&p1, T2 &&p2) const {
-            using Ret = std::common_type_t<T1, T2>;
-            return std::abs(Ret{p1} - Ret{p2});
-        }
-    };
-} // distance_fn
+namespace distance_fn {
+
+template <typename T1 = void, typename T2 = void>
+struct l1_norm {
+    constexpr auto operator()(const T1 &p1, const T2 p2) const {
+        using Ret = std::common_type_t<T1, T2>;
+        return std::abs(Ret{p1} - Ret{p2});
+    }
+};
+
+template <>
+struct l1_norm<void, void> {
+    template <typename T1, typename T2>
+    constexpr auto operator()(T1 &&p1, T2 &&p2) const {
+        return l1_norm<T1, T2>()(p1, p2);
+    }
+};
+}  // namespace distance_fn
